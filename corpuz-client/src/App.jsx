16e04ab4246f816c6fb1026c0853c6ed89f15c27
rafaelcorpuz-z <johnrafaelcorpuz00@gmail.com>
2026-03-13 15:26:24 +0800

    lab-act1

--- corpuz-client/src/App.jsx
+++ corpuz-client/src/App.jsx
@@ -6,8 +6,8 @@ import './App.css'
 
 function App() {
   return (
-    <div>className="App"
-      <header> className="App-header"
+    <div className="App">
+      <header className="App-header">
         <h1>Welcome to My React App!</h1>
         <p>
           Name: John Rafael P. Corpuz
